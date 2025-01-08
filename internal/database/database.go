7e9e72f69451e68a502152c0422b308bdeada06b
hj235 <hongjing235@gmail.com>
2025-01-09 02:19:34 +0800

    Connect server to local MySQL database

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,8 +1,40 @@
 package database
 
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
 type Database struct {
+	db *sql.DB
 }
 
+func InitialiseDB() *sql.DB {
+	pwd := os.Getenv("MYSQL_PWD")
+	db, err := sql.Open("mysql", "root:"+pwd+"@tcp(localhost:3306)/webforum")
+	if err != nil {
+		fmt.Println("An error has occurred with the data source name.\nOpening a driver will typically not attempt to connect to the database.")
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("An error has occurred connecting to the database with the given DSN.")
+		log.Fatal(err)
+	}
+
+	fmt.Println("Successfully connected to database.")
+
+	return db
+}
+
+var DBInstance Database = Database{InitialiseDB()}
+
 func GetDB() (*Database, error) {
-	return &Database{}, nil
+
+	return &DBInstance, nil
 }
