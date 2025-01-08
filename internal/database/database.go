f64ca25a24d399f6f91b7c4ef712af9c5cfef6a3
hj235 <hongjing235@gmail.com>
2025-01-09 03:25:49 +0800

    List all users

--- internal/database/database.go
+++ internal/database/database.go
@@ -10,10 +10,11 @@ import (
 )
 
 type Database struct {
-	db *sql.DB
+	Database *sql.DB
 }
 
 func InitialiseDB() *sql.DB {
+	// Verify DSN
 	pwd := os.Getenv("MYSQL_PWD")
 	db, err := sql.Open("mysql", "root:"+pwd+"@tcp(localhost:3306)/webforum")
 	if err != nil {
@@ -21,6 +22,7 @@ func InitialiseDB() *sql.DB {
 		log.Fatal(err)
 	}
 
+	// Verify Database connection
 	err = db.Ping()
 	if err != nil {
 		fmt.Println("An error has occurred connecting to the database with the given DSN.")
@@ -35,6 +37,10 @@ func InitialiseDB() *sql.DB {
 var DBInstance Database = Database{InitialiseDB()}
 
 func GetDB() (*Database, error) {
-
+	// Verify Database connection
+	err := DBInstance.Database.Ping()
+	if err != nil {
+		return nil, err
+	}
 	return &DBInstance, nil
 }
